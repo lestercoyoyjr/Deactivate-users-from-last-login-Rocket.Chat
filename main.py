070b27dd1b37be2c5e2f2592729c9a7e381f403d
lestercoyoy <lester_cch@hotmail.com>
2023-10-03 20:57:29 -0600

    MODIFY
    Add the code to deactivate user
    Ask if it wants to deactivate it.

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
 # Read official documentation here: https://developer.rocket.chat/reference/api/rest-api/endpoints/user-management/users-endpoints/get-users-info
 
-import requests
+import requests, os
+from dotenv import load_dotenv
+from datetime import datetime
 
-# Define the API URL and headers
+load_dotenv()
+
 url = "http://localhost:3000/api/v1/users.info"
+
 headers = {
-    "X-Auth-Token": "LriN8m6slB0GpY5GUrcx3IizyTRUpIxWS21XEu37gl5",
+    "X-Auth-Token": os.getenv("X-Auth-Token"),
+    "X-User-Id": os.getenv("X-User-Id"),
     "Content-Type": "application/json",
-    "X-User-Id": "k4vsvoirPcnvwkvrB"
 }
 
-# Define the query parameter (in this case, 'username')
 query_params = {
     "userId": "6Ft8E9onv5RjT7DNT"
 }
 
-# Make the GET request
+# To deactivate user
+payload = {
+    'activeStatus': False,  # Set to False to deactivate the user
+    'userId': '6Ft8E9onv5RjT7DNT',
+    'confirmRelinquish': True
+}
+
+
 response = requests.get(url, headers=headers, params=query_params)
 
-# Check if the request was successful (status code 200)
 if response.status_code == 200:
-    # Parse the JSON response
     data = response.json()
-    
-    # Check what data is coming in the GET call
-    print(data, "\n")
-    
-    # Extract the 'lastLogin' parameter
     last_login = data.get('user', {}).get('lastLogin')
+    username = data.get('user', {}).get('username')
     
-    if last_login:
-        print("Last Login:", last_login)
+    # Calculate the current date
+    current_date = datetime.utcnow()
+    
+    last_login_date = datetime.strptime(last_login, '%Y-%m-%dT%H:%M:%S.%fZ')
+    days_difference = (current_date - last_login_date).days
+    
+    if days_difference > 30: # could be any number
+        user_input = input("The user " + username + " last login is older than 30 days. Do you want to deactivate this user? (yes/no): ")
+        
+        if user_input.lower() == "yes" or user_input.lower() == "y":
+            # Code to deactivate the user
+            url2 = "http://localhost:3000/api/v1/users.setActiveStatus"
+            response2 = requests.post(url2, headers=headers, json=payload)
+            if response2.status_code == 200:
+                data = response2.json()
+                if data.get('success'):
+                    user = data.get('user')
+                    print("The user " + username + " has been deactivated.")
+                else:
+                    print("Failed to set user status")
+        else:
+            print("The user " + username + " will not be deactivated.")
     else:
-        print("Last Login not found in the response.")
+        print("The last login is not older than 30 days.")
 else:
     print("Failed to retrieve data. Status code:", response.status_code)
     print(response.text)
