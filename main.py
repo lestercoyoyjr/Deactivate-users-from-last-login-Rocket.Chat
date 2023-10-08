318a42caebcc4e4bab357abe0f2ea1a659cf6605
lestercoyoy <lester_cch@hotmail.com>
2023-10-07 22:56:27 -0600

    MODIFY
    def get_current_date() -> difference_days()
    
    All functionality from days be passed to the function

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 
-def get_current_date():
-    return datetime.utcnow()
+def difference_days(last_login):
+    current_date = datetime.utcnow()
+    last_login_date = datetime.strptime(last_login, '%Y-%m-%dT%H:%M:%S.%fZ')
+    days_difference = (current_date - last_login_date).days
+    return days_difference
 
 def deactivate_user(headers, payload, username):
     url = "http://localhost:3000/api/v1/users.setActiveStatus"
@@ -46,10 +49,7 @@ def main():
         username = data.get('user', {}).get('username')
 
         # Calculate the current date
-        current_date = get_current_date()
-
-        last_login_date = datetime.strptime(last_login, '%Y-%m-%dT%H:%M:%S.%fZ')
-        days_difference = (current_date - last_login_date).days
+        days_difference = difference_days(last_login)
 
         if days_difference > 30:  # could be any number
             user_input = input(
