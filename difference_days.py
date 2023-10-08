6688cf79a48a606c7f084b515381a1a10242e7bf
lestercoyoy <lester_cch@hotmail.com>
2023-10-08 01:28:02 -0600

    MODIFY
    in order to not allow a None value

--- difference_days.py
+++ difference_days.py
@@ -3,5 +3,6 @@ from datetime import datetime
 def difference_days(last_login):
     current_date = datetime.utcnow()
     last_login_date = datetime.strptime(last_login, '%Y-%m-%dT%H:%M:%S.%fZ')
-    days_difference = (current_date - last_login_date).days
-    return days_difference
+    if last_login_date != None:
+        days_difference = (current_date - last_login_date).days
+        return days_difference
